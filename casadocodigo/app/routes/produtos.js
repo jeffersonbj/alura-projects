c13a86312c4bc13429e9f71a3a850085005dceac
jeffersonbj <jeffersonbj@gmail.com>
2017-06-20 10:46:14 -0300

    Add files via upload

---
+++ casadocodigo/app/routes/produtos.js
@@ -0,0 +1,14 @@
+module.exports = function (app) {
+    app.get('/produtos', function (req, res) {
+        
+        var connection = app.infra.connectionFactory();
+        var produtosBanco = app.infra.produtosBanco;
+
+        produtosBanco.lista(connection, function (erros, resultados) {            
+            res.render('produtos/lista', { lista: resultados });
+        });
+
+        connection.end();        
+    });
+
+}
